d54eb087c66c259617a76c5465b3888d47d9149d
github username <github email address>
2018-10-20 10:59:18 -0400

    truly hate github

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import ConversationsList from './components/ConversationsList';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      userId: null,
+    }
+  }
+
+  fetchUser = (userId) => {
+    fetch('https://localhost3000/users')
+
+    this.setState({
+      userId
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <ConversationsList />
+
+        {this.state.userId ? <ConversationsList user={this.state.userId}/> : <Login logIn={this.fetchUser}/>}
       </div>
     );
   }
