5ff8d2485adbb4f4211028794a9fa32ea18188ba
github username <github email address>
2018-10-23 15:31:48 -0400

    changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = "https://hidden-fortress-58788.herokuapp.com/";
-export const API_WS_ROOT = 'ws://hidden-fortress-58788.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://hidden-fortress-58788.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
