82863a2862d9da92b2b47f995894b8ef9fda168a
github username <github email address>
2019-01-05 10:01:11 -0500

    changed urls

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const ICE = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
 
-export const API_ROOT = "https://ba844288.ngrok.io/";
-export const API_WS_ROOT = 'wss:ba844288.ngrok.io/cable';
+export const API_ROOT = "https://a93f916c.ngrok.io/";
+export const API_WS_ROOT = 'a93f916c.ngrok.io/cable';
 // export const API_ROOT = "https://gentle-refuge-17696.herokuapp.com/";
 // export const API_WS_ROOT = 'wss://gentle-refuge-17696.herokuapp.com/cable';
 export const HEADERS = {
