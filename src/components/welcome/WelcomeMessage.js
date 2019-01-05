c83327dba77c2639fadfde883ec7ba0c6a83952f
github username <github email address>
2019-01-04 20:19:29 -0500

    edited welcome message

--- src/components/welcome/WelcomeMessage.js
+++ src/components/welcome/WelcomeMessage.js
@@ -8,13 +8,13 @@ const WelcomeMessage = () => {
       <div className="welcome-message" style={{"paddingLeft": "10%", "paddingTop": "10%"}}>
         <h3 className="ui header">Welcome to Skypish!</h3>
 
-        <p>This is a video sharing and live messaging application. It allows two users to video chat and send each other messages.</p>
+        <p>This is a video chat and messaging application.</p>
 
-        <p>*The app needs a little cosmetic work and some polishing. The bulk of it was built in a 3 day code sprint. The video funtion works first try 80% of the time. If it doesn't work on first try, try clicking the video button from the other user, the user who did not initiate the video chat. It also needs some polishing - user creation/validation, etc*</p>
+        <p>*The app needs a little cosmetic work and some polishing. The bulk of it was built in a 3 day code sprint. The video function works first try 90% of the time. If it doesn't work on first try, try clicking the video button from the other user, the user who did not initiate the video chat. It also needs some polishing - user creation/validation, etc*</p>
 
-        <p>Auth was implimented but its commented out in the App.js file. When testing on a single computer, it makes it little cumbersome to log into two profiles.</p>
+        <p>Auth was implemented but its commented out in the App.js file. When testing on a single computer, it makes it little cumbersome to log into two profiles.</p>
 
-        <p>To test the app you can login with the two users listed below two tabs with the two users listed below, or on two separate computers.</p>
+        <p>To test the app you can login with the two users listed below in two tabs with the two users listed below, or on two separate computers.</p>
 
         <ul>
           <li>eben@eben.com</li>
